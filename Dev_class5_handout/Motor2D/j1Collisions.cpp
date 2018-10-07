ef2e184eb70d1ea02e07ddfc59f4558035af3a1c
FBarnes99 <ferranbarnes@gmail.com>
2018-10-07 14:43:51 +0200

    Preparing Collision modules to implementation

---
+++ Dev_class5_handout/Motor2D/j1Collisions.cpp
@@ -0,0 +1,71 @@
+#include "j1Collisions.h"
+#include "p2Defs.h"
+#include "j1Module.h"
+#include "j1App.h"
+#include "j1Input.h"
+#include "j1Render.h"
+#include "p2Log.h"
+#include "j1Map.h"
+#include "SDL\include\SDL.h"
+
+j1Collisions::j1Collisions() : j1Module()
+{
+
+	for (uint i = 0; i < MAX_NUM_COLLIDERS; ++i) {
+		colliders[i] = nullptr;
+	}
+	name.create("collisions");
+
+	matrix[PLAYER_COLLIDER][PLAYER_COLLIDER] = false;
+	matrix[PLAYER_COLLIDER][GROUND_COLLIDER] = true;
+	
+	matrix[GROUND_COLLIDER][PLAYER_COLLIDER] = true;
+	matrix[GROUND_COLLIDER][GROUND_COLLIDER] = false;
+	
+}
+
+j1Collisions::~j1Collisions() {}
+
+bool j1Collisions::PreUpdate()
+{
+	
+}
+
+bool j1Collisions::CleanUp()
+{
+	
+}
+
+bool j1Collisions::Update(float dt) {
+
+	
+}
+
+void j1Collisions::Collider_to_debug() {
+
+	
+}
+
+Collider* j1Collisions::AddCollider(SDL_Rect rect, COLLIDER_TYPE type, j1Module* callback)
+{
+	
+}
+uint j1Collisions::Colliders_Count() const {
+
+
+}
+bool j1Collisions::Delete_Collider(Collider* collider)
+{
+	
+}
+
+bool Collider::Check_Collision(const SDL_Rect& r) const {
+
+
+}
+
+CollisionDirection Collider::Check_Direction(const SDL_Rect& r) const {
+
+	
+}
+
---
+++ Dev_class5_handout/Motor2D/j1Collisions.h
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef __MODULECOLLISIONS_H__
+#define __MODULECOLLISIONS_H__
+
+#include "j1Module.h"
+#include "p2Point.h"
+#include "SDL\include\SDL_rect.h"
+
+#define MAX_NUM_COLLIDERS 800
+
+enum COLLIDER_TYPE
+{
+	NO_COLLIDER = 0,
+	PLAYER_COLLIDER,
+	GROUND_COLLIDER,
+	CHECK_COLLIDER,
+
+};
+
+enum CollisionDirection
+{
+	NO_COLLISION = 0,
+	FROM_ABOVE,
+	FROM_BELOW,
+	FROM_RIGHT,
+	FROM_LEFT
+};
+
+struct Collider
+{
+	SDL_Rect rect;
+	bool to_delete = false;
+	COLLIDER_TYPE type;
+
+
+	j1Module* callback = nullptr;
+
+	Collider(SDL_Rect rectangle, COLLIDER_TYPE type, j1Module* callback = nullptr) : rect(rectangle), type(type), callback(callback) {}
+
+	void Set_Pos(int x, int y) { rect.x = x; rect.y = y; }
+	bool Check_Collision(const SDL_Rect& r) const;
+	CollisionDirection Check_Direction(const SDL_Rect& r) const;
+};
+
+class j1Collisions : public j1Module
+{
+public:
+
+	j1Collisions();
+	~j1Collisions();
+
+	bool PreUpdate();
+	bool Update(float dt);
+	bool CleanUp();
+	uint Colliders_Count() const;
+
+	bool Delete_Collider(Collider* collider);
+	Collider* AddCollider(SDL_Rect rect, COLLIDER_TYPE type, j1Module* callback = nullptr);
+	void Collider_to_debug();
+	bool debug = false;
+
+
+private:
+
+	Collider* colliders[MAX_NUM_COLLIDERS];
+	bool matrix[CHECK_COLLIDER][CHECK_COLLIDER];
+};
+
+#endif// __MODULECOLLISION_H__
